992bb397f0abe57dc2a07bc9a76d8373976b4df3
Mithun Das <mithun.das@coxautoinc.com>
2018-03-01 17:14:42 -0500

    first commit

--- handler.js
+++ handler.js
@@ -4,6 +4,11 @@ const AWS = require('aws-sdk');
 AWS.config.update({region:'us-east-1'});
 const rekognition = new AWS.Rekognition();
 
+var config = {
+  "awsRegion":"us-east-1",
+  "s3Bucket":"raspi118528",
+  "awsFaceCollection":"raspifacecollection"
+}
 module.exports.bot = (event, context, callback) => {
   try {
         // By default, treat the user request as coming from the America/New_York time zone.
@@ -30,6 +35,32 @@ module.exports.bot = (event, context, callback) => {
                           console.log('body',body);
 
                           if(body.status ==='matched' || body.status ==='error'){
+                            var hour = new Date().getHours();
+                            var content="";
+
+                            if(hour >= 5 && hour<12){
+                              content ="Good morning, "+ body.message+" !. Have a nice day!";
+                            }else if(hour >=12 && hour <18){
+                              content ="Good afternoon, "+ body.message+" !. Have a nice rest of the day!";
+                            }else if(hour >=18 && hour < 21){
+                              content ="Good evening, "+ body.message+" !. Talk to you soon!";
+                            }else{
+                              content ="Hi, "+ body.message+" !. It's been a long day! Good Night!";
+                            }
+
+                            callback(null,{
+                                sessionAttributes: {key: body.key},
+                                dialogAction: {
+                                    type: 'Close',
+                                    fulfillmentState:'Fulfilled',
+                                    message:{
+                                        contentType: 'PlainText',
+                                        content: content
+                                    }
+
+                                },
+                            })
+                          }else if(body.status ==='error'){
                             callback(null,{
                                 sessionAttributes: {key: body.key},
                                 dialogAction: {
@@ -68,12 +99,12 @@ module.exports.bot = (event, context, callback) => {
                     var name = event.currentIntent.slots.name;
                     var key = event.sessionAttributes.key;
                     var params = {
-                        "CollectionId": "raspifacecollection",
+                        "CollectionId": config.awsFaceCollection,
                         "DetectionAttributes": [ "ALL" ],
                         "ExternalImageId": name,
                         "Image": {
                           "S3Object": {
-                             "Bucket": "raspi118528",
+                             "Bucket": config.s3Bucket,
                              "Name": key
                           }
                         }
@@ -101,6 +132,66 @@ module.exports.bot = (event, context, callback) => {
                     });
 
               break;
+
+              case 'ClearAllFacesIntent':
+                var params = {
+                    "CollectionId": config.awsFaceCollection,
+                    "MaxResults": 20
+                }
+
+                rekognition.listFaces(params, function(err, data) {
+                    if (err) console.log(err, err.stack); // an error occurred
+                      else     {
+                      console.log(data);
+                      var faceIds=[];
+                      data.Faces.forEach(function(face){
+                        faceIds.push(face.FaceId);
+                      })
+
+                    if(faceIds.length>0){
+                      var params = {
+                      CollectionId: config.awsFaceCollection,
+                      FaceIds: faceIds
+                      };
+                      rekognition.deleteFaces(params, function(err, data) {
+
+                        if (err) {
+                          callback(null,{
+                              sessionAttributes: {key: key},
+                              dialogAction: {
+                                  type: 'Close',
+                                  fulfillmentState:'Fulfilled',
+                                  message:{
+                                      contentType: 'PlainText',
+                                      content: "Something went wrong! I could not delete faces!"
+                                  }
+
+                              },
+                          })
+                        }
+                        else   {
+                          callback(null,{
+                              sessionAttributes: {key: key},
+                              dialogAction: {
+                                  type: 'Close',
+                                  fulfillmentState:'Fulfilled',
+                                  message:{
+                                      contentType: 'PlainText',
+                                      content: "I have deleted all the faces from memory!"
+                                  }
+
+                              },
+                          })
+                        }
+
+                      });
+                    }
+
+                    }
+
+                 });
+
+                    break;
           default:
 
         }
